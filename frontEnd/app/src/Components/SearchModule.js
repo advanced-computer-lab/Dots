cebe4ddc73956b3bfa56f3aba3e12b970c7d9096
FIRST_NAME:LAST_NAME <maroushdy2@gmail.com>
2021-10-31 17:58:29 +0200

    Installed Material UI, react hot loader, created first search module

---
+++ frontEnd/app/src/Components/SearchModule.js
@@ -0,0 +1,19 @@
+import React from "react";
+import TextField from '@mui/material/TextField';
+import {Component} from 'react';
+
+class SearchModule extends Component{
+
+    render()
+    {
+        return <div>
+                  <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+
+        </div>
+
+
+    }
+}
+
+
+export default SearchModule;
