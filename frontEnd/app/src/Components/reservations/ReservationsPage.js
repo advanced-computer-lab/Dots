7c68f5b657a9ef7bd4d632cca5705710c4bab6ff
Omar Elsawi <omarelsawi98@gmail.com>
2021-11-26 20:06:01 +0200

    Add some files for reservations

---
+++ frontEnd/app/src/Components/reservations/ReservationsPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReservationsList from './components/ReservationsList'
+
+class ReservationsPage extends Component {
+    render() {
+        return (
+            <ReservationsList/>
+        );
+    }
+}
+
+export default ReservationsPage;
---
+++ frontEnd/app/src/Components/reservations/components/ReservationCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ReservationCard extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default ReservationCard;
