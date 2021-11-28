291d8b704954c1bebfa728f3093e47d1b3d7ff63
Omar Elsawi <omarelsawi98@gmail.com>
2021-11-28 14:07:18 +0200

    Add some features to reservation card

--- frontEnd/app/src/Components/reservations/components/ReservationCard.js
+++ frontEnd/app/src/Components/reservations/components/ReservationCard.js
@@ -7,13 +7,15 @@ class ReservationCard extends Component {
         const tempArr = [1, 2, 3]
         var cardStyle = {
             height: '15vw',
+            borderRadius: '1vw'
         }
         return (
-            <Stack  sx={{ mx: 5, my:15 }} spacing={2}>
+            <Stack  sx={{ mx: 40, my:15 }} spacing={7}>
                 {tempArr.map(() => (
-                <Card style={cardStyle}>
+                <Card style={cardStyle} elevation="7">
                     <CardHeader title="Flight Qw123e"/>
-                    <Divider />
+
+                    <Divider style={{width:'100%'}} />
                 </Card>
                 ))}
             </Stack>
